386203b66b4ea0efab98447ab5addda62a11be00
Aditya <adityasharma10102@gmail.com>
2024-05-31 16:50:07 +0530

    minor changes

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_KEY = 'AIzaSyCAdyniYRoNZ7lFXGEUd9_EbvVBcxOma6c';
 const SHEET_ID = '1OOgwbnliog4CYujyhpcSsYtNK4XRaHnie_EQKKf6svE';
-const RANGE = 'Sheet1!A1:D10'; // Adjust the range according to your sheet
+const RANGE = 'Sheet1!A1:D10'; 
 
 export const fetchData = async () => {
   const url = `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${RANGE}?key=${API_KEY}`;
